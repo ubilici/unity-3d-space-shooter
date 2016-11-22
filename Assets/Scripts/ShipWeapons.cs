37c586d40d3a519a3475999aee1fbda0bf5bceb1
Uğurhan Bilici <ubilici94@gmail.com>
2016-11-22 19:28:15 +0300

    add weapon and indicator scripts

---
+++ Assets/Scripts/ShipWeapons.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipWeapons : MonoBehaviour
+{
+    public GameObject shotPrefab;
+    public Transform[] firePoints;
+
+    private int firePointIndex;
+
+    public void Awake()
+    {
+        InputManager.instance.SetWeapons(this);
+    }
+
+    public void OnDestroy()
+    {
+        if(Application.isPlaying == true)
+        {
+            InputManager.instance.RemoveWeapons(this);
+        }
+    }
+
+    public void Fire()
+    {
+        if(firePoints.Length == 0)
+        {
+            return;
+        }
+
+        var firePointToUse = firePoints[firePointIndex];
+
+        Instantiate(shotPrefab, firePointToUse.position, firePointToUse.rotation);
+
+        firePointIndex++;
+
+        if(firePointIndex >= firePoints.Length)
+        {
+            firePointIndex = 0;
+        }
+    }
+}
